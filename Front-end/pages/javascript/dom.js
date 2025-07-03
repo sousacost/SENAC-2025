140ba2c0b64e52e818c62ebb57f16b9062220060
sousacost <antoniosousaw22@gmail.com>
2025-07-02 21:36:21 -0300

    card com imagem e textos, e aplicação do json
    Efeitar card, e aplicando json e lista

--- Front-end/pages/javascript/dom.js
+++ Front-end/pages/javascript/dom.js
@@ -14,5 +14,24 @@ elemento.className = 'btn' //define uma classe ao elemetno, .classlist
 
 //container.innerHTML += `<button class="btn"> Literal</button>`
 //container.innerHTML += `<button class="${texto}"> ${Literal}</button>`
-texto = `Literal`
-containex.innerHTML += `<button class="${texto}"> ${Literal}</button>`
+
+var container = document.getElementById(`containex`)
+
+container.innerHTML = `
+        <Div class="aa">
+
+            <img class="top"
+                src="https://image.discovery.indazn.com/ca/v2/ca/image?id=2qbw8h5rftc6ih6bdrxc0dupd_image-header_pRow_1750863072000&quality=50"
+                alt="220" width="200">
+
+            <img class="circulo"
+                src="https://th.bing.com/th/id/R.31b17a5f971bd2d35a0e98856ab151fb?rik=c9%2fczsMPO%2fu0Ow&riu=http%3a%2f%2fminutoligado.com.br%2fwp-content%2fuploads%2f2014%2f04%2fwallpaper-flamengo.jpg&ehk=8mw2Bn7iW5neb0gCpZOvVYw%2bGdP6sR%2fuYTuVfrvRU%2bA%3d&risl=&pid=ImgRaw&r=0"
+                alt="100" width="100" height="">
+
+            <div class="text">
+                <p>
+                <h3>Flamengo perde de 4x2 para o poderoso Bayern. </p>
+            </div>
+        
+        </Div>
+`
---
+++ Front-end/scipts/json.js
@@ -0,0 +1,45 @@
+//json - Javascript object nonation
+
+var pessoa = {
+"nome": "Lena",
+"foto":"http/...",
+"post":"htpp:/",
+"Descrição":"text"
+
+
+}
+console.log(pessoa.nome) //para acessar atributos do objeto
+// ultiliza-se  o operador "." seguido da chave. Ex `pessoa.nome´
+
+pessoa.nome = "Luana" //para editar o atributo de um objeto, acessamos
+//o atributo e ultilizamos o operador de atribuição "=" para definir o novo valor
+
+var produto ={
+    "nome":"celular",
+    "id":" 0",
+    "descrição":" descrição",
+    "preco":"1000.00"
+}
+
+var produto = [
+    {
+    "nome":"celular1",
+    "id":" 1",
+    "descrição":"Jogos",
+    "preco":"1600.00"
+},
+{
+    "nome":"celular2",
+    "id":" 2",
+    "descrição":"Trabalho",
+    "preco":"1070.00"
+},
+{
+    "nome":"celular3",
+    "id":" 3",
+    "descrição":" Estudos",
+    "preco":"1066.00"
+}
+
+]
+console.log(produtos[2]) //para acessar um item da lista ultilizando o index(lista[x])
