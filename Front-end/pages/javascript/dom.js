ebd6c3ddcc45a2e0ca11136cbd9b2e52451fc178
sousacost <antoniosousaw22@gmail.com>
2025-07-03 21:06:15 -0300

    Multiplos cards
    Cards com imagens

--- Front-end/pages/javascript/dom.js
+++ Front-end/pages/javascript/dom.js
@@ -1,6 +1,6 @@
 
 //document.writeln("HELLO WORD!");
-var container = document.getElementById("container")
+var container = document.getElementById("containex")
 var elemento = document.createElement('button') //ceiando um botão  
 
 elemento.textContent = "Aperte" //define o conteudo textualdo elemento
@@ -8,30 +8,53 @@ elemento.textContent = "Aperte" //define o conteudo textualdo elemento
 elemento.id =  'btn' // define um "id" ao elemento.
 elemento.className = 'btn' //define uma classe ao elemetno, .classlist 
 // também pode ser usado se for adicionar mais de uma classe
-// container.appendChild(elemento) // adiciona um elemento filho
-// texto = `Literal`
-// texto = `bnt`
 
-//container.innerHTML += `<button class="btn"> Literal</button>`
-//container.innerHTML += `<button class="${texto}"> ${Literal}</button>`
+function Acao(){
+    alert("Ação")
+}
+container.appendChild(elemento) // adiciona um elemento filho
+ Literal = `Literal`
+ texto = `bnt`
 
-var container = document.getElementById(`containex`)
+container.innerHTML += `<button class="btn"> Literal</button>`
+container.innerHTML += `<button class="${texto}"> ${Literal}</button>`
 
-container.innerHTML = `
+
+var pessoa =[
+{
+"nome": "Jason",
+"foto":"https://tse2.mm.bing.net/th/id/OIP.kH4rwTrPd7EvfcGxGlPg9gHaEK?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+"post":"https://tse3.mm.bing.net/th/id/OIP.AO94syirkeI9IIUROXlu1gHaEO?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+"descrição":"Jason"
+},
+{
+"nome": "Atores invocação",
+"foto": "https://tse2.mm.bing.net/th/id/OIP.6I87zGmf3YqycLrcfuU9UgAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+"post":"https://uploads.metropoles.com/wp-content/uploads/2016/06/09113341/THE-CONJURING-2-11.jpg",
+"descrição":"Filme conpleto na Netiflix"
+},
+{
+"nome": "Drop:Ameaça anonima",
+"foto":"https://cinepop.com.br/wp-content/uploads/2025/01/drop1-1024x683.webp",
+"post":"https://cinepop.com.br/wp-content/uploads/2025/01/drop3-1024x683.webp",
+"descrição":"subgênero do terror e tecnologia"
+},
+
+]
+
+for(let i = 0; i < pessoa.length; i++){
+    container.innerHTML += `
         <Div class="aa">
 
             <img class="top"
-                src="https://image.discovery.indazn.com/ca/v2/ca/image?id=2qbw8h5rftc6ih6bdrxc0dupd_image-header_pRow_1750863072000&quality=50"
-                alt="220" width="200">
+                src="${pessoa[i].post}" alt="" width="300" height="200"></img>
 
-            <img class="circulo"
-                src="https://th.bing.com/th/id/R.31b17a5f971bd2d35a0e98856ab151fb?rik=c9%2fczsMPO%2fu0Ow&riu=http%3a%2f%2fminutoligado.com.br%2fwp-content%2fuploads%2f2014%2f04%2fwallpaper-flamengo.jpg&ehk=8mw2Bn7iW5neb0gCpZOvVYw%2bGdP6sR%2fuYTuVfrvRU%2bA%3d&risl=&pid=ImgRaw&r=0"
-                alt="100" width="100" height="">
-
-            <div class="text">
-                <p>
-                <h3>Flamengo perde de 4x2 para o poderoso Bayern. </p>
-            </div>
-        
+               <img class="circulo"
+                src="${pessoa[i].foto}" alt="" width="50" height="50"></img>
+                
+                <p>"${pessoa[i].descrição}"</p>
+             
         </Div>
 `
+
+}
