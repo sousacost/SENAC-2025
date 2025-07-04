64ba27bd344054eef6955980dcacfb3147a0fbb0
sousacost <antoniosousaw22@gmail.com>
2025-07-03 21:56:29 -0300

    jogo
    primeiro jogo

---
+++ Front-end/pages/javascript/jogo.js
@@ -0,0 +1,10 @@
+ square = document.createElement("div")
+ square.className = "square"
+
+document.body.appendChild(square)
+
+square.addEventListener("click", ()=> {
+    let a = square
+    document.body.appendChild(a)
+
+ })
