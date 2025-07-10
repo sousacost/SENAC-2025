2cbe20bbe34cda0a9bc5f47c6bc4af43980e9100
sousacost <antoniosousaw22@gmail.com>
2025-07-09 22:00:37 -0300

    formulário e strings
    strings

---
+++ Front-end/scipts/strings.js
@@ -0,0 +1,18 @@
+/*var text = 'O rato roeu a roupa do rei de roma'
+var palavras = text.split('')
+for(let i = 0; i < palavras.length; i++){
+    alert(palavras[i])
+    if("rato" === palavras[i]){
+        palavras[i] = 'cachorro'
+    }
+}*/
+//alert(palavras.join('').replace('roma', 'Grécia'))
+var soma = 0
+for(let i = 0;i <  palavras.length;i++){
+    if("o" === palavras[i]){
+        soma++
+    }
+
+}
+alert(soma)
+alert(text)
